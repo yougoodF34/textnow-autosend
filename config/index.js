c24eed81aa66e937bd1a8455e52a7f9397bdb76d
yougoodF34 <101261954+yougoodF34@users.noreply.github.com>
2022-03-11 21:49:00 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  username: process.env.TEXTNOW_USERNAME || "",
-  password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
+  username: process.env.TEXTNOW_USERNAME || "DomenicaWeissenbachchc71@gmail.com",
+  password: process.env.TEXTNOW_PASSWORD || "PP6966pa",
+  recipient: process.env.TEXTNOW_RECIPIENT || "14044886799",
   message: process.env.TEXTNOW_MESSAGE || "autosend message",
   cookies_str: process.env.TEXTNOW_COOKIES || ""
 };
